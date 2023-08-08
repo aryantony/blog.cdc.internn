fa03968cb50281513eb9b8bad69549d9d5eaf4af
Aryan <aryanroy3865@gmail.com>
2023-08-08 21:23:02 +0530

    all done

---
+++ main.js
@@ -0,0 +1,9 @@
+// let x = window.matchMedia("(min-width:0px) and (max-width:621px)");
+// myfun(x);
+// x.addListener(myfun);
+// function myfun(x) {
+//   if (x.matches) {
+//     document.querySelector(".ise_heading").innerHTML = `<h1 class="ise_heading"><span class="okj">Securing an Internship at</span><span class="orcnm">
+//     Oracle</span><span class="okj1">: My Journey and Insights</span></h1>`;
+//   }
+// }
